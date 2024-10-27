cc1432d6d5f9428611f4953ebbeba8b95ad7382e
Andrey Fedyukov <afedukov@gmail.com>
2024-10-27 08:56:34 +0100

    Model metrics: model column width
    (cherry picked from commit 0d6881a524efa7a4ed42637bfbc040cbf9da1e71)

--- resources/views/livewire/pages/models.blade.php
+++ resources/views/livewire/pages/models.blade.php
@@ -51,7 +51,7 @@
 						<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 							<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 							<tr>
-								<th scope="col" class="px-4 py-3 min-w-64">
+								<th scope="col" class="px-4 py-3 min-w-52">
 									{{ __('Model') }}
 								</th>
 								<th scope="col" class="px-4 py-3 min-w-64">
