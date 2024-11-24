e24e59931cb3e50360c8f64000131de8167684fe
Andrey Fedyukov <afedukov@gmail.com>
2024-11-24 14:01:41 +0100

    Filter archived style fix
    (cherry picked from commit 8b60dec4ddbec411e2928bbba8c1f83ee7727663)

--- resources/views/livewire/evaluations/filter-archived.blade.php
+++ resources/views/livewire/evaluations/filter-archived.blade.php
@@ -1,6 +1,6 @@
 <div class="relative flex">
 
-	<div class="inline-flex rounded-md" role="group">
+	<div class="inline-flex rounded-md items-center" role="group">
 		<div class="flex">
 			<input type="radio" wire:model.live="filter" key="archived-all" name="filter-archived" id="archived-all" value="all" class="hidden peer" />
 			<label for="archived-all" class="px-4 py-2 cursor-pointer peer-checked:z-10 peer-checked:ring-2 peer-checked:ring-blue-700 peer-checked:text-blue-700 dark:peer-checked:ring-blue-500 dark:peer-checked:text-white text-xs font-semibold text-slate-400 dark:text-slate-400 uppercase bg-white border border-gray-200 rounded-s-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
